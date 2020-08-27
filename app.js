05a2cfc2e8d5b01bc94f7811bbdba6f612bfd267
jaisam <jaisamtani123@gmail.com>
2020-08-27 23:33:43 +0530

    Added mongoose client and code refactoring

--- app.js
+++ app.js
@@ -1,80 +1,106 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 const url = require('url');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-let links = [];
-let linksObject = [];
+let limit = 5;
+let globalLinksArray = [];
+let globalLinksObjectArray = [];
 
 
-const getLinks = (response) => { 
-    const $ = cheerio.load(response.data);
-    const anchortags = $('a');   
-    $(anchortags).each(function(i,tag){
-        const link = $(tag).attr('href');
-        if(link.startsWith('https://medium.com')) {
+// Connecting to Database
+mongoose.connect(process.env.db_url,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+})
+.then(async () => {
+    console.log(`Connected to DB!`);
 
-            // Create Object
-            const url = new URL(link);
-            const baseUrl = url.origin + url.pathname;
-            let params = [];
-            url.searchParams.forEach((value, property) => {
-                params.push(property);
-            });
-
-            // Check if object exists
-            const presentLink = linksObject.find(object => {
-                if(object.url == baseUrl){
-                    return object;
-                }
-            });
-
-            // Update old object
-            if(presentLink){
-                presentLink.count++;
-                params.forEach( param => {
-                    if(!presentLink.params.includes(param))
-                        presentLink.params.push(param);
-                });
-            // Insert newly created Object
-            } else {
-                linksObject.push({
-                    url : baseUrl,
-                    count : 1,
-                    params : params
-                });
-            }
-            links.push(link);
-        }
-    });
-    console.log(links);
-    console.log(linksObject);
-};
+    // Main Function Call
+    hyperlinkCrawler();
+})
+.catch(err => {
+    console.log('**** err in mongoose connect ****');
+    console.log(err)
+});
 
 
-const hyperlinkCrawler = (url) => {
+const crawlAndParseUrl = (url) => { 
+    return new Promise(function (resolve, reject) {
         axios.get(url)
         .then(response => {
-            getLinks(response);
-            let promises = [];
-            for(let i = 0 ; i < links.length ; i++){
-                if(promises.length < 5) {
-                    //check in db
-                    promises.push(axios.get(links[i]));
-                }
-                else {
-                    Promise.all(promises)
-                    .then(results => {
-                        results.forEach(result => {
-                            getLinks(result);
-                        });
-                        i--;
-                        promises = [];
+            const $ = cheerio.load(response.data);
+            const anchortags = $('a');
+            $(anchortags).each(function(i,tag){
+                const link = $(tag).attr('href');
+                if(link.startsWith('https://medium.com')) {
+ 
+                    // Create URL Object
+                    const newUrl = new URL(link);
+                    const linkObj = { 
+                    url : newUrl.origin + newUrl.pathname,
+                    count : 1,
+                    params : []
+                    };
+                    newUrl.searchParams.forEach((value, property) => {
+                        linkObj.params.push(property);
                     });
+ 
+                    // Check if object exists
+                    let existingLinkObj = globalLinksObjectArray.find(object => {
+                        if(object.url == linkObj.url){
+                            return object;
+                        }
+                    });
+
+                    let paramMismatch = false;
+                    if(existingLinkObj){
+                        // Update old object  
+                        existingLinkObj.count++;
+                        linkObj.params.forEach( param => {
+                            if(!existingLinkObj.params.includes(param)) {
+                                existingLinkObj.params.push(param);
+                                paramMismatch = true;
+                            }
+                        }); 
+                    } else {
+                        // Insert newly created Object
+                        globalLinksObjectArray.push(linkObj);
+                        paramMismatch = true;
+                    }
+ 
+                    if(paramMismatch) globalLinksArray.push(link);
+ 
                 }
-            }
-        });
+            });
+            console.log('************** globalLinksArray **************', globalLinksArray);
+            console.log('************** globalLinksObjectArray **************', globalLinksObjectArray);
+            
+            return resolve();
+        })
+        .catch(err => {
+            console.log('******** err in crawlAndPraseUrl ********');
+            console.log(err);
+            return reject(err);
+        })
+    });
 };
 
-// Main Function Call
-hyperlinkCrawler(process.env.url);
+
+const hyperlinkCrawler = async (url) => {
+    let  urlPromisesList = [];
+    globalLinksArray.push(process.env.url);
+    
+    for(let i = 0 ; i <= globalLinksArray.length ; i++){
+        if( urlPromisesList.length >= limit || i == globalLinksArray.length ) {
+            const response = await Promise.all(urlPromisesList);
+                i--;
+                urlPromisesList = [];
+        }
+        else {
+            urlPromisesList.push(crawlAndParseUrl(globalLinksArray[i]));
+        }
+    }
+};
