26e6499c0086207c3a39b2af7d7b8652542e7a5b
jaisam <jaisamtani123@gmail.com>
2020-08-28 17:02:51 +0530

    Added try catch

--- app.js
+++ app.js
@@ -37,118 +37,123 @@ mongoose.connect(process.env.db_url,
 
 // Helper Function
 const crawlAndParseUrl = async (url) => {
-    let paramMismatch = false;
-
-    console.log('----------------------------------------');
-    console.log('Before await Link.findOne ', url);
-
-    let dbLinkObj = await Link.findOne({
-        url: url
-    });
-
-    let dbPromise;
-    
-    console.log('+++++++++++++++++++++++++++++++++++++++++');
-    console.log('After await Link.findOne', url);
-    console.log('dbLinkObj ', dbLinkObj);
-
-    // Create URL Object
-    const newUrl = new URL(url);
-    const linkObj = { 
-        url : newUrl.origin + newUrl.pathname,
-        count : 1,
-        params : []
-    };
-    newUrl.searchParams.forEach((value, property) => {
-        linkObj.params.push(property);
-    });
-
-    // console.log('linkObj ', linkObj);
-
-    if(dbLinkObj) {
-        // Update old object  
-        dbLinkObj.count++;
-        linkObj.params.forEach( param => {
-            if(!dbLinkObj.params.includes(param)) {
-                dbLinkObj.params.push(param);
-                paramMismatch = true;
-            }
-        });
-        dbPromise = dbLinkObj.save();
-        console.log('Saving Object', dbPromise);
-    } else {
-        paramMismatch = true;
-        dbPromise = Link.create(linkObj);
-        console.log('Creating Object ', dbPromise);
-    }
-    
-    console.log('-------------------------------------');
-    console.log('Before await dbPromise ', url);
-
-    // return dbPromise;
-    const record = await dbPromise;
-
-    console.log('+++++++++++++++++++++++++++++++++++++++++');
-    console.log('After awaiting dbPromise ', url );
-    console.log('record =>', record);
-    console.log('paramMismatch =>', paramMismatch);
-
-    if(paramMismatch){
+    try {
+        let paramMismatch = false;
 
         console.log('----------------------------------------');
-        console.log('Before await axios', url);
+        console.log('Before await Link.findOne ', url);
 
-        let response = await axios.get(url);
-        
-        console.log('+++++++++++++++++++++++++++++++++++++++++');
-        console.log('After await axios', url);
-
-        const $ = cheerio.load(response.data);
-        const anchortags = $('a');
-        $(anchortags).each(function(i,tag){
-            const link = $(tag).attr('href');
-            if(link.startsWith('https://medium.com')) {
-    
-                // Create URL Object
-                // const newUrl = new URL(link);
-                // const linkObj = { 
-                // url : newUrl.origin + newUrl.pathname,
-                // count : 1,
-                // params : []
-                // };
-                // newUrl.searchParams.forEach((value, property) => {
-                //     linkObj.params.push(property);
-                // });
-    
-                // Check if object exists
-                // let existingLinkObj = globalLinksObjectArray.find(object => {
-                //     if(object.url == linkObj.url){
-                //         return object;
-                //     }
-                // });
-    
-                // let paramMismatch = false;
-                // if(existingLinkObj){
-                //     // Update old object  
-                //     existingLinkObj.count++;
-                //     linkObj.params.forEach( param => {
-                //         if(!existingLinkObj.params.includes(param)) {
-                //             existingLinkObj.params.push(param);
-                //             paramMismatch = true;
-                //         }
-                //     }); 
-                // } else {
-                //     // Insert newly created Object
-                //     globalLinksObjectArray.push(linkObj);
-                //     paramMismatch = true;
-                // }
-    
-                globalLinksArray.push(link);
-            }
+        let dbLinkObj = await Link.findOne({
+            url: url
         });
 
-        console.log('************** globalLinksArray **************', globalLinksArray);
-        // console.log('************** globalLinksObjectArray **************', globalLinksObjectArray);
+        let dbPromise;
+        
+        console.log('+++++++++++++++++++++++++++++++++++++++++');
+        console.log('After await Link.findOne', url);
+        console.log('dbLinkObj ', dbLinkObj);
+
+        // Create URL Object
+        const newUrl = new URL(url);
+        const linkObj = { 
+            url : newUrl.origin + newUrl.pathname,
+            count : 1,
+            params : []
+        };
+        newUrl.searchParams.forEach((value, property) => {
+            linkObj.params.push(property);
+        });
+
+        // console.log('linkObj ', linkObj);
+
+        if(dbLinkObj) {
+            // Update old object  
+            dbLinkObj.count++;
+            linkObj.params.forEach( param => {
+                if(!dbLinkObj.params.includes(param)) {
+                    dbLinkObj.params.push(param);
+                    paramMismatch = true;
+                }
+            });
+            dbPromise = dbLinkObj.save();
+            console.log('Saving Object', dbPromise);
+        } else {
+            paramMismatch = true;
+            dbPromise = Link.create(linkObj);
+            console.log('Creating Object ', dbPromise);
+        }
+        
+        console.log('-------------------------------------');
+        console.log('Before await dbPromise ', url);
+
+        // return dbPromise;
+        const record = await dbPromise;
+
+        console.log('+++++++++++++++++++++++++++++++++++++++++');
+        console.log('After awaiting dbPromise ', url );
+        console.log('record =>', record);
+        console.log('paramMismatch =>', paramMismatch);
+
+        if(paramMismatch){
+
+            console.log('----------------------------------------');
+            console.log('Before await axios', url);
+
+            let response = await axios.get(url);
+            
+            console.log('+++++++++++++++++++++++++++++++++++++++++');
+            console.log('After await axios', url);
+
+            const $ = cheerio.load(response.data);
+            const anchortags = $('a');
+            $(anchortags).each(function(i,tag){
+                const link = $(tag).attr('href');
+                if(link.startsWith('https://medium.com')) {
+        
+                    // Create URL Object
+                    // const newUrl = new URL(link);
+                    // const linkObj = { 
+                    // url : newUrl.origin + newUrl.pathname,
+                    // count : 1,
+                    // params : []
+                    // };
+                    // newUrl.searchParams.forEach((value, property) => {
+                    //     linkObj.params.push(property);
+                    // });
+        
+                    // Check if object exists
+                    // let existingLinkObj = globalLinksObjectArray.find(object => {
+                    //     if(object.url == linkObj.url){
+                    //         return object;
+                    //     }
+                    // });
+        
+                    // let paramMismatch = false;
+                    // if(existingLinkObj){
+                    //     // Update old object  
+                    //     existingLinkObj.count++;
+                    //     linkObj.params.forEach( param => {
+                    //         if(!existingLinkObj.params.includes(param)) {
+                    //             existingLinkObj.params.push(param);
+                    //             paramMismatch = true;
+                    //         }
+                    //     }); 
+                    // } else {
+                    //     // Insert newly created Object
+                    //     globalLinksObjectArray.push(linkObj);
+                    //     paramMismatch = true;
+                    // }
+        
+                    globalLinksArray.push(link);
+                }
+            });
+
+            console.log('************** globalLinksArray **************', globalLinksArray);
+            // console.log('************** globalLinksObjectArray **************', globalLinksObjectArray);
+        }
+    }
+    catch(err) {
+        console.log(err);
     }
 };
 
